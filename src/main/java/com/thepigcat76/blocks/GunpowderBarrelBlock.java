b5efec63b70cbe5218347116825ffc336fbbd1a2
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-06-12 19:24:13 +0200

    removed wrenching from gunpowder_barrel

--- src/main/java/com/thepigcat76/blocks/GunpowderBarrelBlock.java
+++ src/main/java/com/thepigcat76/blocks/GunpowderBarrelBlock.java
@@ -1,49 +1,9 @@
 package com.thepigcat76.blocks;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.TntBlock;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import reborncore.api.ToolManager;
-import reborncore.common.blocks.BlockWrenchEventHandler;
-import reborncore.common.util.WrenchUtils;
 
 public class GunpowderBarrelBlock extends TntBlock {
     public GunpowderBarrelBlock(Settings settings) {
         super(settings);
-        BlockWrenchEventHandler.wrenchableBlocks.add(this);
-    }
-
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        ItemStack stack = player.getStackInHand(hand);
-        if (!stack.isOf(Items.FLINT_AND_STEEL) && !stack.isOf(Items.FIRE_CHARGE)) {
-            return super.onUse(state, world, pos, player, hand, hit);
-        } else {
-            primeTnt(world, pos);
-            world.setBlockState(pos, Blocks.AIR.getDefaultState(), 11);
-            Item item = stack.getItem();
-            if (!player.isCreative()) {
-                if (stack.isOf(Items.FLINT_AND_STEEL)) {
-                    stack.damage(1, player, (playerx) -> {
-                        playerx.sendToolBreakStatus(hand);
-                    });
-                } else {
-                    stack.decrement(1);
-                }
-            }
-            if (!stack.isEmpty() && ToolManager.INSTANCE.canHandleTool(stack) && WrenchUtils.handleWrench(stack, world, pos, player, hit.getSide())) {
-                return ActionResult.SUCCESS;
-            } else {
-                return ActionResult.PASS;
-            }
-        }
     }
 }
